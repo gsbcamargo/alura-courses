b35ae265924591b3ddb22f857f8b5c7bdc7c7cf6
Gabriel Schreiner Bueno de Camargo <gsbcamargo@gmail.com>
2021-05-12 19:00:17 -0300

    created user model

---
+++ gerenciador/src/br/com/gabriel/gerenciador/model/User.java
@@ -0,0 +1,32 @@
+package br.com.gabriel.gerenciador.model;
+
+public class User {
+	
+	private String login;
+	private String password;
+	
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public boolean equals(String login, String password) {
+		if(!this.login.equals(login)) {
+			return false;
+		} 
+		
+		if(!this.password.equals(password)) {
+			return false;
+		}
+		return true;
+	}
+
+}
