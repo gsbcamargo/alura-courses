bce3a04304d57440cdd20531527e3c12edc525cb
Gabriel Schreiner Bueno de Camargo <gsbcamargo@gmail.com>
2021-05-13 20:16:13 -0300

    added companies service

--- gerenciador/src/br/com/gabriel/gerenciador/servlet/AuthorizationFilter.java
+++ gerenciador/src/br/com/gabriel/gerenciador/servlet/AuthorizationFilter.java
@@ -3,6 +3,7 @@ package br.com.gabriel.gerenciador.servlet;
 import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -16,6 +17,14 @@ import javax.servlet.http.HttpSession;
  */
 //@WebFilter("/input")
 public class AuthorizationFilter implements Filter {
+	
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+	
+	@Override
+	public void destroy() {
+	}
 
 	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
 		
--- gerenciador/src/br/com/gabriel/gerenciador/servlet/ControllerFilter.java
+++ gerenciador/src/br/com/gabriel/gerenciador/servlet/ControllerFilter.java
@@ -3,6 +3,7 @@ package br.com.gabriel.gerenciador.servlet;
 import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
@@ -18,6 +19,14 @@ import br.com.gabriel.gerenciador.action.Action;
  */
 //@WebFilter("/input")
 public class ControllerFilter implements Filter {
+	
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+	
+	@Override
+	public void destroy() {
+	}
 
 	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
 		
