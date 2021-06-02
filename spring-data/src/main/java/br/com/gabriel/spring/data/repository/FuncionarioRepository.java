28f8431b2a532d71cd18888d4383decee6c7ae86
Gabriel Schreiner Bueno de Camargo <gsbcamargo@gmail.com>
2021-06-02 11:51:17 -0300

    created HQL query in funcionarioRepository to search for name, salary and hiring date

--- spring-data/src/main/java/br/com/gabriel/spring/data/repository/FuncionarioRepository.java
+++ spring-data/src/main/java/br/com/gabriel/spring/data/repository/FuncionarioRepository.java
@@ -1,7 +1,9 @@
 package br.com.gabriel.spring.data.repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +13,9 @@ import br.com.gabriel.spring.data.orm.Funcionario;
 public interface FuncionarioRepository extends CrudRepository<Funcionario, Integer> {
 	
 	List<Funcionario> findByNome(String nome);
+	
+	@Query("SELECT f FROM Funcionario f WHERE f.nome = :nome "
+			+ "AND f.salario >= :salario "
+			+ "AND f.dataContratacao = :data")
+	List<Funcionario> buscaNomeSalarioMaiorQueEDataContratacao(String nome, Double salario, LocalDate data);
 }
--- spring-data/src/main/java/br/com/gabriel/spring/data/service/RelatoriosService.java
+++ spring-data/src/main/java/br/com/gabriel/spring/data/service/RelatoriosService.java
@@ -1,5 +1,7 @@
 package br.com.gabriel.spring.data.service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Scanner;
 
@@ -13,6 +15,8 @@ public class RelatoriosService {
 
 	private Boolean system = true;
 	
+	private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+	
 	private final FuncionarioRepository funcionarioRepository;
 	
 	public RelatoriosService(FuncionarioRepository funcionarioRepository) {
@@ -22,15 +26,19 @@ public class RelatoriosService {
 	public void inicial(Scanner scanner) {
 		
 		while (system) {
-			System.out.println("Qual ação referente a cargos deseja executar?");
+			System.out.println("Qual relatório deseja executar?");
 			System.out.println("0 - Sair");
 			System.out.println("1 - Busca funcionário por nome");
-		
+			System.out.println("2 - Busca funcionário por nome, salario maior que... e data de contratação");
+			
 			int action = scanner.nextInt();
 			
 			switch (action) {
 			case 1:
 				buscaFuncionarioPorNome(scanner);
+				break;
+			case 2:
+				buscaNomeSalarioMaiorQueEDataContratacao(scanner);
 				break;
 			default:
 				system = false;
@@ -40,9 +48,27 @@ public class RelatoriosService {
 	}
 	
 	private void buscaFuncionarioPorNome(Scanner scanner) {
-		System.out.println("Digite o nome a ser pesquisado: ");
+		System.out.println("Digite o nome a ser pesquisado:");
 		String nome = scanner.next();
 		List<Funcionario> lista = funcionarioRepository.findByNome(nome);
 		lista.forEach(System.out::println);
+	}
+	
+	private void buscaNomeSalarioMaiorQueEDataContratacao(Scanner scanner) {
+		System.out.println("Digite o nome a ser pesquisado:");
+		String nome = scanner.next();
+		
+		System.out.println("Digite a data de contratação a ser pesquisada:");
+		String data = scanner.next();
+		
+		LocalDate localDate = LocalDate.parse(data, formatter);
+		
+		System.out.println("Digite o salário a ser pesquisado:");
+		Double salario = scanner.nextDouble();
+		
+		List<Funcionario> list = funcionarioRepository
+				.buscaNomeSalarioMaiorQueEDataContratacao(nome, salario, localDate);
+		list.forEach(System.out::println);
+		
 	}
 }
