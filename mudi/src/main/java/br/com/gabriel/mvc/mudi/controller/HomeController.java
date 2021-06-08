dc8bb388e8b209709099cd1686b81063dbeed555
Gabriel Schreiner Bueno de Camargo <gsbcamargo@gmail.com>
2021-06-08 20:05:27 -0300

    added mysql and jpa dependecies

--- mudi/src/main/java/br/com/gabriel/mvc/mudi/controller/HomeController.java
+++ mudi/src/main/java/br/com/gabriel/mvc/mudi/controller/HomeController.java
@@ -1,7 +1,10 @@
 package br.com.gabriel.mvc.mudi.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,17 +15,15 @@ import br.com.gabriel.mvc.mudi.model.Pedido;
 @Controller
 public class HomeController {
 
+	@PersistenceContext
+	private EntityManager em;
+	
 	@RequestMapping("/home")
 	public String home(Model model) {
-		Pedido pedido = new Pedido();
+		
+		Query query = em.createQuery("SELECT p FROM Pedido p", Pedido.class);
+		List<Pedido> pedidos = query.getResultList();
 
-		pedido.setNomeProduto("Kindle");
-		pedido.setUrlImagem("https://images-na.ssl-images-amazon.com/images/I/61X0ISBpD-L._AC_SL1000_.jpg");
-		pedido.setUrlProduto(
-				"https://www.amazon.com.br/Kindle-10a-gera%C3%A7%C3%A3o-ilumina%C3%A7%C3%A3o-embutida/dp/B07FQK1TS9/ref=asc_df_B07FQK1TS9/?tag=googleshopp00-20&linkCode=df0&hvadid=432951822456&hvpos=&hvnetw=g&hvrand=17413742439289834887&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1001634&hvtargid=pla-901759904091&psc=1");
-		pedido.setDescricao("Anything really just for testing purposes");
-
-		List<Pedido> pedidos = Arrays.asList(pedido, pedido, pedido, pedido);
 		model.addAttribute("pedidos", pedidos);
 
 		return "home";
