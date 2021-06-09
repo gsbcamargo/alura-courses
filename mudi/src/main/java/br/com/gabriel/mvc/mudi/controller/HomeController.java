a7f5ae7e75d5a84044bbe6f37320fbb62a29464a
Gabriel Schreiner Bueno de Camargo <gsbcamargo@gmail.com>
2021-06-09 18:46:37 -0300

    created form page, form logic, dto

--- mudi/src/main/java/br/com/gabriel/mvc/mudi/controller/HomeController.java
+++ mudi/src/main/java/br/com/gabriel/mvc/mudi/controller/HomeController.java
@@ -2,30 +2,25 @@ package br.com.gabriel.mvc.mudi.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.gabriel.mvc.mudi.model.Pedido;
+import br.com.gabriel.mvc.mudi.repository.PedidoRepository;
 
 @Controller
 public class HomeController {
 
-	@PersistenceContext
-	private EntityManager em;
-	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+
 	@RequestMapping("/home")
 	public String home(Model model) {
-		
-		Query query = em.createQuery("SELECT p FROM Pedido p", Pedido.class);
-		List<Pedido> pedidos = query.getResultList();
 
+		List<Pedido> pedidos = pedidoRepository.findAll();
 		model.addAttribute("pedidos", pedidos);
-
 		return "home";
 	}
 
---
+++ mudi/src/main/java/br/com/gabriel/mvc/mudi/repository/PedidoRepository.java
@@ -0,0 +1,14 @@
+package br.com.gabriel.mvc.mudi.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.gabriel.mvc.mudi.model.Pedido;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+
+
+}
