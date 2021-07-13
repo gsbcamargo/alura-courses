8c06d92793e7cdb533c455fc667b7e192300cb52
Gabriel Schreiner Bueno de Camargo <gsbcamargo@gmail.com>
2021-07-13 19:52:50 -0300

    created findByName method in topico repository

--- forum/src/main/java/br/com/alura/forum/repository/TopicoRepository.java
+++ forum/src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -1,11 +1,14 @@
 package br.com.alura.forum.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.alura.forum.modelo.Topico;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
-	
-	
 
+	List<Topico> findByCursoNome(String nomeCurso); // the underline means that spring will understand this 
+													 // as "topic -> relation Curso -> nome attribute from Curso
+	
 }
