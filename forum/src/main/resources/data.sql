5105e8eb28ddb301a253854c9915a389f14025ca
Gabriel Schreiner Bueno de Camargo <gsbcamargo@gmail.com>
2021-07-21 16:40:39 -0300

    configured authentication

--- forum/src/main/resources/data.sql
+++ forum/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$oj9KW0U7V/H53WUC5VhSsu3o9BsdSRNU/9Yx8T/R7c76bAj7BxH7C');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
