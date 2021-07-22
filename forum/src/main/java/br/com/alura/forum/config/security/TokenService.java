96facd4581d813d812e923eef28d3ae6bd32f0ae
Gabriel Schreiner Bueno de Camargo <gsbcamargo@gmail.com>
2021-07-22 17:53:04 -0300

    configured access token (jwt)

--- forum/src/main/java/br/com/alura/forum/config/security/TokenService.java
+++ forum/src/main/java/br/com/alura/forum/config/security/TokenService.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import br.com.alura.forum.modelo.Usuario;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -28,8 +29,24 @@ public class TokenService {
 				.setIssuer("API do Forum da Alura")
 				.setSubject(logado.getId().toString())
 				.setIssuedAt(hoje)
-				.setExpiration(hoje)
+				.setExpiration(dataExpiracao)
 				.signWith(SignatureAlgorithm.HS256, secret)
 				.compact();
 	}
+
+	public boolean isTokenValido(String token) {
+		
+		try {
+			Jwts.parser().setSigningKey(this.secret).parseClaimsJws(token);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	public Long getIdUsuario(String token) {
+		Claims claims = Jwts.parser().setSigningKey(this.secret).parseClaimsJws(token).getBody();
+		return Long.parseLong(claims.getSubject());
+	}
+	
 }
