8a5020fd67261cc9607c2339f305260f723f4e6b
Gabriel Schreiner Bueno de Camargo <gsbcamargo@gmail.com>
2021-07-26 11:21:14 -0300

    created admin profile authorized to delete topics

--- forum/src/main/resources/data.sql
+++ forum/src/main/resources/data.sql
@@ -1,4 +1,12 @@
 INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$oj9KW0U7V/H53WUC5VhSsu3o9BsdSRNU/9Yx8T/R7c76bAj7BxH7C');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Moderador', 'moderador@email.com', '$2a$10$oj9KW0U7V/H53WUC5VhSsu3o9BsdSRNU/9Yx8T/R7c76bAj7BxH7C');
+
+INSERT INTO PERFIL(id, nome) VALUES (1, 'ROLE_ALUNO');
+INSERT INTO PERFIL(id, nome) VALUES (2, 'ROLE_MODERADOR');
+
+INSERT INTO USUARIO_PERFIS(usuario_id, perfis_id) VALUES(1, 1);
+INSERT INTO USUARIO_PERFIS(usuario_id, perfis_id) VALUES(2, 2);
+
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
