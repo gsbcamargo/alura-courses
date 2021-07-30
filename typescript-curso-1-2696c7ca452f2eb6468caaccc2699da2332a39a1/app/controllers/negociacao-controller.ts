6863340b4bbde5ea29a0f11ed9be8fe436acda0b
Gabriel Schreiner Bueno de Camargo <gsbcamargo@gmail.com>
2021-07-30 16:35:36 -0300

    i actually forgot what I did ffs

--- typescript-curso-1-2696c7ca452f2eb6468caaccc2699da2332a39a1/app/controllers/negociacao-controller.ts
+++ typescript-curso-1-2696c7ca452f2eb6468caaccc2699da2332a39a1/app/controllers/negociacao-controller.ts
@@ -1,29 +1,35 @@
-import { Negociacao } from "../models/negociacao.js";
-import { Negociacoes } from "../models/negociacoes.js";
-import { NegociacoesView } from "../views/negociacoes-view.js";
+import { Negociacao } from '../models/negociacao.js';
+import { Negociacoes } from '../models/negociacoes.js';
+
 export class NegociacaoController {
+    private inputData: HTMLInputElement;
+    private inputQuantidade: HTMLInputElement;
+    private inputValor: HTMLInputElement;
+    private negociacoes = new Negociacoes();
+
     constructor() {
-        this.negociacoes = new Negociacoes();
-        this.negociacoesView = new NegociacoesView('#negociacoesView');
         this.inputData = document.querySelector('#data');
         this.inputQuantidade = document.querySelector('#quantidade');
         this.inputValor = document.querySelector('#valor');
-        this.negociacoesView.update(this.negociacoes);
     }
-    adiciona() {
+
+    adiciona(): void {
         const negociacao = this.criaNegociacao();
+        negociacao.data.setDate(12);
         this.negociacoes.adiciona(negociacao);
-        this.negociacoesView.update(this.negociacoes);
+        console.log(this.negociacoes.lista());
         this.limparFormulario();
     }
-    criaNegociacao() {
+
+    criaNegociacao(): Negociacao {
         const exp = /-/g;
         const date = new Date(this.inputData.value.replace(exp, ','));
         const quantidade = parseInt(this.inputQuantidade.value);
         const valor = parseFloat(this.inputValor.value);
         return new Negociacao(date, quantidade, valor);
     }
-    limparFormulario() {
+
+    limparFormulario(): void {
         this.inputData.value = '';
         this.inputQuantidade.value = '';
         this.inputValor.value = '';
