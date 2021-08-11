83501791fe976ea3f1e7e675e1ede304f9e0b7a6
Gabriel Schreiner Bueno de Camargo <gsbcamargo@gmail.com>
2021-08-11 11:08:27 -0300

    added protected modifier in methods

--- typescript-curso-1/app/models/negociacoes.ts
+++ typescript-curso-1/app/models/negociacoes.ts
@@ -4,11 +4,11 @@ export class Negociacoes {
     private negociacoes: Negociacao[] = [];
 
 
-    adiciona(negociacao: Negociacao) {
+    public adiciona(negociacao: Negociacao) {
         this.negociacoes.push(negociacao);
     }
 
-    listar(): readonly Negociacao[] {
+    public listar(): readonly Negociacao[] {
         // return [...this.negociacoes]; // put the list items into a new list so changes cannot be performed in the latter
         return this.negociacoes;
     }
--- typescript-curso-1/app/views/negociacoes-view.ts
+++ typescript-curso-1/app/views/negociacoes-view.ts
@@ -3,7 +3,7 @@ import { View } from "./view.js";
 
 export class NegociacoesView extends View<Negociacoes> {
 
-    template(model: Negociacoes): string {
+    protected template(model: Negociacoes): string {
         return `
             <table class="table" table-hover table-bordered">
                 <thead>
@@ -17,7 +17,7 @@ export class NegociacoesView extends View<Negociacoes> {
                     ${model.listar().map(negociacao => {
                         return `
                             <tr>
-                                <td>${new Intl.DateTimeFormat().format(negociacao.data)}</td>
+                                <td>${this.formatarData(negociacao.data)}</td>
                                 <td>${negociacao.quantidade}</td>
                                 <td>${negociacao.valor}</td>
                             </tr>
@@ -27,5 +27,9 @@ export class NegociacoesView extends View<Negociacoes> {
                 </tbody>
             </table>
         `;
+    }
+
+    private formatarData(data: Date) {
+        return new Intl.DateTimeFormat().format(data);
     }
 }
