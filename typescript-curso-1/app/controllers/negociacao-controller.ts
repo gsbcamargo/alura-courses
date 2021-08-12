a981194bafdbe4f0ba5a66f30224facf5785edab
Gabriel Schreiner Bueno de Camargo <gsbcamargo@gmail.com>
2021-08-12 18:51:04 -0300

    created static method criaDe to prevent class instantiation

--- typescript-curso-1/app/controllers/negociacao-controller.ts
+++ typescript-curso-1/app/controllers/negociacao-controller.ts
@@ -22,7 +22,12 @@ export class NegociacaoController {
     }
 
     public adiciona(): void {
-        const negociacao = this.criaNegociacao();
+        const negociacaoTemp = new Negociacao(null, 0, 0);
+        const negociacao = Negociacao.criaDe(
+            this.inputData.value,
+            this.inputQuantidade.value,
+            this.inputValor.value
+        );
         if (!this.isDiaUtil(negociacao.data)) {
             this.mensagemView
                 .update('São aceitas apenas negociações em dias úteis.');
@@ -36,14 +41,6 @@ export class NegociacaoController {
 
     private isDiaUtil(data: Date): boolean {
         return data.getDay() > DiasDaSemana.DOMINGO && data.getDay() < DiasDaSemana.SABADO;
-    }
-
-    private criaNegociacao(): Negociacao {
-        const exp = /-/g;
-        const date = new Date(this.inputData.value.replace(exp, ','));
-        const quantidade = parseInt(this.inputQuantidade.value);
-        const valor = parseFloat(this.inputValor.value);
-        return new Negociacao(date, quantidade, valor);
     }
 
     private limparFormulario(): void {
--- typescript-curso-1/app/models/negociacao.ts
+++ typescript-curso-1/app/models/negociacao.ts
@@ -15,4 +15,13 @@ export class Negociacao {
         return this._data;
     }
 
+    // todo método estático (sempre public) é um método que pode ser chamado diretamente na classe, não há necessidade de instanciação
+    public static criaDe(dataString: string, quantidadeString: string, valorString: string): Negociacao { 
+        const exp = /-/g;                                                                                  
+        const date = new Date(dataString.replace(exp, ','));
+        const quantidade = parseInt(quantidadeString);
+        const valor = parseFloat(valorString);
+        return new Negociacao(date, quantidade, valor);
+    }
+
 }
