6e05bce0a5675f7552d269de2ed84e33c2d22ff0
Gabriel Schreiner Bueno de Camargo <gsbcamargo@gmail.com>
2021-08-12 19:15:35 -0300

    added security layer in order to prevent <script> tags inside html

--- typescript-curso-1/app/views/view.ts
+++ typescript-curso-1/app/views/view.ts
@@ -3,14 +3,21 @@ import { Negociacoes } from "../models/negociacoes";
 export abstract class View<T> {
 
     protected elemento: HTMLElement;
+    private escapar = false;
 
-    constructor(seletor: string) {
+    constructor(seletor: string, escapar?: boolean) {
         this.elemento = document.querySelector(seletor);
+        if(escapar) {
+            this.escapar = escapar;
+        }
     }
 
     public update(model: T): void {
-        const template = this.template(model);
+        let template = this.template(model);
         this.elemento.innerHTML = template;
+        if (this.escapar) {
+            template = template.replace(/<script>[\s\S]*?<\/script>/, '');
+        }
     }
 
     protected abstract template(model: T): string;
